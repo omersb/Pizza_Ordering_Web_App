7af3ee428c8168aafaee7086ef0cd2b966c9b4c7
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-03 15:54:54 +0300

    update

--- pizza/models.py
+++ pizza/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Size(models.Model):
     SIZE = [
         ('Small', 'Small'),
@@ -14,11 +12,10 @@ class Size(models.Model):
     def __str__(self):
         return self.title
 
-
 class Pizza(models.Model):
     topping1 = models.CharField('Topping_1', max_length=30)
     topping2 = models.CharField('Topping_2', max_length=30)
-    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size,on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.size} Pizza with {self.topping1} and {self.topping2} '
--- pizza/urls.py
+++ pizza/urls.py
@@ -5,5 +5,5 @@ from pizza.views import home, ordering_pizza,multi_order
 urlpatterns = [
     path("", home, name='home'),
     path("order/", ordering_pizza, name='order'),
-    path("multiorder/", multi_order, name='order'),
+    path("multiorder/", multi_order, name='multiorder'),
 ]
